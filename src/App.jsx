6d7b88faadf286bce69e19a8532d345eb31fd5fd
Rajatbhatt1310 <rajatbhatt1310@gmail.com>
2026-05-04 10:07:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+return (
+    <>
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </>
+  );
+}
+
+export default App;
